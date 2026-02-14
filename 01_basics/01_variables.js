ba13f6bb8d70aeabfe472d8dd539f9b898f201bc
Rohan <rohan.muni.24@gmail.com>
2026-02-14 16:36:45 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 144553
+//accountId = 7
+
+let accEmail = "rohan@gmail.com"
+var accPassword = "123456"
+accCity = "Banglore"
+let accState
+
+
+accEmail = "abk.abvure"
+accPassword = "567543"
+accCity = "Japan"
+/*
+will not use var cause of some issue in function scope and block scope
+*/
+console.log(accountId);
+console.table([accountId,accEmail,accPassword,accCity,accState])
+ 
