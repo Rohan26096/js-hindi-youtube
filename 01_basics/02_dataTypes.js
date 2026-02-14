e45a3bdff45069db51c473033f5d4f33fe61b787
Rohan <rohan.muni.24@gmail.com>
2026-02-14 16:34:07 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,18 @@
+"use strict";
+console.log(3+3);
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
+
+//ECMA script 
+
+/*
+number 2
+string ""
+bigint doesnt use
+boolean true/false
+null sstand alone value
+undefined 
+symbol find uniques
+
+
+*/
